ba3896f126a11878aea618c0e66a367e85d8c3f3
Mustafa Zorlu <mzorlu3579@gmail.com>
2022-10-08 00:39:32 +0300

    changed desc txt

--- easy.js
+++ easy.js
@@ -1,3 +1,7 @@
 import { userValidation } from "./easy/userValidation.js";
 
-console.log(userValidation("u__hello_world123"));
+// console.log(userValidation("u__hello_world123"));
+
+import { longestWord } from "./easy/longestWord.js";
+
+console.log(longestWord("fun&!! time"));
